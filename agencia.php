bf7c63d82041403b3d2a651095e276a0340a5af5
BenjaminPazos <20251FOZ0020031@estudantes.ifpr.edu.br>
2025-12-03 15:49:54 -0300

    agencia.php

--- agencia.php
+++ agencia.php
@@ -1,19 +1,20 @@
 <?php
 
 const CHEQUE_ESPECIAL = 500;
-$clientes = []; //global
+$clientes = [];
 
 function cadastrarCliente(&$clientes): bool
 {
 
     $nome = readline('Informe seu nome: ');
-    $cpf = readline('Informe seu CPF: ');
+    $cpf  = readline('Informe seu CPF: ');
 
     //validar cliente
-    if (isset($cliente[$cpf])) {
-        print('Esse CPF já possui cadastro');
+    if (isset($clientes[$cpf])) {
+        print('Esse CPF já possui cadastro.\n');
         return false;
     }
+
     $clientes[$cpf] = [
         'nome' => $nome,
         'cpf' => $cpf,
@@ -25,9 +26,10 @@ function cadastrarCliente(&$clientes): bool
 
 function cadastrarConta(array &$clientes): bool
 {
-    $cpf = readline("Informe seu CPF: ");
 
-    if (! isset($cliente[$cpf])) {
+    $cpf = readline("Informe seu CPF:");
+
+    if (!isset($clientes[$cpf])) {
         print "Cliente não possui cadastro \n";
         return false;
     }
@@ -36,12 +38,12 @@ function cadastrarConta(array &$clientes): bool
 
     $clientes[$cpf]['contas'][$numConta] = [
         'saldo' => 0,
-        'chequeEspecial' => CHEQUE_ESPECIAL,
+        'cheque_especial' => CHEQUE_ESPECIAL,
         'extrato' => []
     ];
 
     print "Conta criada com sucesso\n";
-    print "Número da sua conta: $numConta\n";
+    print "O número da sua conta é: $numConta";
     return true;
 }
 
@@ -49,9 +51,9 @@ function depositar(array &$clientes)
 {
     $cpf = readline("Informe seu CPF novamente: ");
 
-    $numConta = readline("Informe o número da conta: ");
+    $numConta = readline("Informe o número da conta:");
 
-    $valorDeposito = (float)readline("Informe o valor do depósito: ");
+    $valorDeposito = (float) readline("Informe o valor do depósito: ");
 
     if ($valorDeposito <= 0) {
         print "Valor de depósito inválido\n";
@@ -61,15 +63,16 @@ function depositar(array &$clientes)
     $clientes[$cpf]['contas'][$numConta]['saldo'] += $valorDeposito;
 
     $dataHora = date('d/m/Y H:i');
-    $clientes[$cpf]['contas'][$numConta]['extrato'][] = "Deposito de R$ $valorDeposito em $dataHora";
+    $clientes[$cpf]['contas'][$numConta]['extrato'][] = "Depósito de R$ $valorDeposito em $dataHora";
 
-    print "Deposito realizado com sucesso\n";
+
+    print "Depósito realizado com sucesso\n";
     return true;
 }
 
+// MENU PRINCIPAL
 function menu()
 {
-
     print str_repeat("=", 20);
     print " MEU BANCO EM PHP ";
     print str_repeat("=", 20) . "\n";
@@ -81,7 +84,46 @@ function menu()
     print "6 - consultar extrato\n";
     print "7 - sair\n";
     print str_repeat("=", 58) . "\n";
-
-
 }
-menu();
+
+
+//PROGRAMA PRINCIPAL
+while (true) {
+
+    menu();
+
+    $opcao = readline();
+
+    switch ($opcao) {
+        case '1':
+            cadastrarCliente($clientes);
+            break;
+        case '2':
+            cadastrarConta($clientes);
+            break;
+        case '3':
+            depositar($clientes);
+            break;
+        case '4':
+            sacar($clientes);
+            break;
+
+        case '7':
+            print "Obrigado por usar nosso banco";
+            die();
+
+        default:
+            print "Opção inválida";
+            break;
+    }
+}
+
+function sacar(&$clientes)
+{
+    $cpf = readline("Informe seu CPF: ");
+    $conta = readline("Informe o número da conta: ");
+    $valorSaque = readline("Informe o valor do saque: ");
+    if ($clientes[$cpf]['contas'][$conta]['saldo'] >=$valorSaque){
+    $clientes[$cpf]['contas'][$conta]['saldo'] -= $valorSaque;
+    }
+}
